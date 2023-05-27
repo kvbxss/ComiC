7582de7821cb01cd416fbada70ae8512e92d7a3b
kvbxss <kubadownar444@gmail.com>
2023-05-27 20:24:20 +0200

    HomeScreen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, SafeAreaView  } from 'react-native'
+import styled from 'styled-components/native'
+import React from 'react'
+
+import { colors } from '../components/colors'
+import CardsSection from '../components/Cards/CardsSection'
 
 const Home = () => {
+  
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <HomeView>
+      <HomeWrapper>
+        <CardsSection />
+      </HomeWrapper>
+    </HomeView>
   )
 }
 
 export default Home
+
+const HomeView = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${colors.dark};
+`
+
+const HomeWrapper = styled(View)`
+  flex: 1;
+`
