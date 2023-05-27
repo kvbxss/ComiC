4ab6fbe9b91beb12311868470439e62fe2f8ef9a
kvbxss <kubadownar444@gmail.com>
2023-05-27 14:07:49 +0200

    splash screen finished
    (animate it)

---
+++ components/Card.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { FunctionComponent } from 'react'
+
+
+const Card: FunctionComponent = () => {
+  return (
+    <View>
+      <Text>Card</Text>
+    </View>
+  )
+}
+
+export default Card
+
