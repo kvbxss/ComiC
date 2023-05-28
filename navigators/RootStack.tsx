a932d7393eae7ee3eba749f40abb885793c2a741
kvbxss <kubadownar444@gmail.com>
2023-05-28 14:51:08 +0200

    navigation fix

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -5,9 +5,10 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 //Screens
-import Welcome from '../screens/Splash'
+import Welcome from '../screens/Welcome'
 import Home from '../screens/Home'
 import Details from '../screens/Details'
+
 
 export type RootStackParamList = {
     Welcome: undefined;
@@ -22,7 +23,7 @@ const RootStack: FunctionComponent = () => {
     <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false}}/>
-            <Stack.Screen name="Home" component={Home} options={{ headerShown: false}}/>
+            <Stack.Screen name="Home" component={Home} options={ {headerShown: false}}/>
             <Stack.Screen name="Details" component={Details}  options={{ headerShown: false}}/>
         </Stack.Navigator>
     </NavigationContainer>
