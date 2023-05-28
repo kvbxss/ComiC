c38d305fd32bd9ab26a2dd9e6ca54c89eb45fabe
kvbxss <kubadownar444@gmail.com>
2023-05-28 20:49:45 +0200

    fix

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -18,19 +18,6 @@ export type RootStackParamList = {
 
 const Stack = createStackNavigator<RootStackParamList>()
 
-const config = {
-    animation: 'spring',
-    config: {
-      stiffness: 1000,
-      damping: 500,
-      mass: 3,
-      overshootClamping: true,
-      restDisplacementThreshold: 0.01,
-      restSpeedThreshold: 0.01,
-    },
-  };
-
-
 const RootStack: FunctionComponent = () => {
     return (
     <NavigationContainer>
